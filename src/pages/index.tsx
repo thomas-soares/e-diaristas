fd2340f5887b6bfef9212043b42f422dd4512bf2
Thomas Soares <thomas__soares@hotmail.com>
2021-06-14 22:11:51 -0300

    Utilizando componente SafeEnviroment.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
 
 export default function Home() {
   return (
     <div>
-
+      <SafeEnvironment />
     </div>
   )
 }
