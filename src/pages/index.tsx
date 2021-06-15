c556b53945be334616b20c8745ac264552bbbc9a
Thomas Soares <thomas__soares@hotmail.com>
2021-06-14 22:46:05 -0300

    Colocando em tela componente de titulo.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
+import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 
 export default function Home() {
   return (
     <div>
       <SafeEnvironment />
+      <PageTitle />
     </div>
   )
 }
