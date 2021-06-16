006f9f1ab2124ae7c0a8df66be2facde7e3f0ae3
Thomas Soares <thomas__soares@hotmail.com>
2021-06-15 23:23:17 -0300

    Estilo do subtitulo.

--- src/ui/components/data-display/PageTitle/PageTitle.style.tsx
+++ src/ui/components/data-display/PageTitle/PageTitle.style.tsx
@@ -4,3 +4,9 @@ export const PageTitleContainer = styled('div')`
   text-align: center;
   margin: 40px 0;
 `;
+
+export const PageTitleStyled = styled('h2')`
+  margin: 0;
+  color: #6B2AEE;
+  font-weight: 600;
+`;
