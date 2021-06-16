e100d191a69d5c6d0a1f05ccfd74aa4c26ace412
Thomas Soares <thomas__soares@hotmail.com>
2021-06-15 23:21:32 -0300

    Estilo do container do titulo.

--- src/ui/components/data-display/PageTitle/PageTitle.style.tsx
+++ src/ui/components/data-display/PageTitle/PageTitle.style.tsx
@@ -1,7 +1,6 @@
 import { experimentalStyled as styled } from '@material-ui/core/styles';
 
 export const PageTitleContainer = styled('div')`
-  text-align: right;
-  padding: 16px 0;
-  font-size: 12px;
+  text-align: center;
+  margin: 40px 0;
 `;
