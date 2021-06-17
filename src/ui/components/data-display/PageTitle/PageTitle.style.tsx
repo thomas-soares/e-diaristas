7f3c159e58603e86ea75d85fcd5738a74366e7fe
Thomas Soares <thomas__soares@hotmail.com>
2021-06-16 22:28:42 -0300

    Desestruturando a prop theme.

--- src/ui/components/data-display/PageTitle/PageTitle.style.tsx
+++ src/ui/components/data-display/PageTitle/PageTitle.style.tsx
@@ -7,6 +7,6 @@ export const PageTitleContainer = styled('div')`
 
 export const PageTitleStyled = styled('h2')`
   margin: 0;
-  color: ${(props) => props.theme.palette.primary.main};
+  color: ${({ theme }) => theme.palette.primary.main};
   font-weight: 600;
 `;
