21492884585f6893d5079b49d7082ec380b0d005
Thomas Soares <thomas__soares@hotmail.com>
2021-06-16 23:09:46 -0300

    Colocando tamanho de font-size no breakpoint md.

--- src/ui/components/data-display/PageTitle/PageTitle.style.tsx
+++ src/ui/components/data-display/PageTitle/PageTitle.style.tsx
@@ -10,4 +10,8 @@ export const PageTitleStyled = styled('h2')`
   color: ${({ theme }) => theme.palette.primary.main};
   font-size: ${({ theme }) => theme.typography.h6.fontSize};
   font-weight: 600;
+
+  ${({ theme }) => theme.breakpoints.down('md')} {
+    font-size: ${({ theme }) => theme.typography.body1.fontSize};
+  }
 `;
