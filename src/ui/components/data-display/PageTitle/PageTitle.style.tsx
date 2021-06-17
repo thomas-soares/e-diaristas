2741a4b0eec86b48771dffbbb3f7c9cd59f11938
Thomas Soares <thomas__soares@hotmail.com>
2021-06-16 22:30:39 -0300

    Pegando font-size do tema.

--- src/ui/components/data-display/PageTitle/PageTitle.style.tsx
+++ src/ui/components/data-display/PageTitle/PageTitle.style.tsx
@@ -8,5 +8,6 @@ export const PageTitleContainer = styled('div')`
 export const PageTitleStyled = styled('h2')`
   margin: 0;
   color: ${({ theme }) => theme.palette.primary.main};
+  font-size: ${({ theme }) => theme.typography.h6.fontSize};
   font-weight: 600;
 `;
