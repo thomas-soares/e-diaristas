6122cbe3914699667e66878e98691286cd7f104e
Thomas Soares <thomas__soares@hotmail.com>
2021-06-16 23:12:39 -0300

    Utilizando o tema pra aplicar tamanho da margin.

--- src/ui/components/data-display/PageTitle/PageTitle.style.tsx
+++ src/ui/components/data-display/PageTitle/PageTitle.style.tsx
@@ -2,7 +2,7 @@ import { experimentalStyled as styled } from '@material-ui/core/styles';
 
 export const PageTitleContainer = styled('div')`
   text-align: center;
-  margin: 40px 0;
+  margin: ${({ theme }) => theme.spacing(5) + ' ' + 0};
 `;
 
 export const PageTitleStyled = styled('h2')`
