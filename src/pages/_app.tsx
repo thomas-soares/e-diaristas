9f511bf70742e3807e012f23d9d7b7b6ae46fd91
Thomas Soares <thomas__soares@hotmail.com>
2021-06-17 22:59:02 -0300

    Importando themeprovider do material ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
+import { ThemeProvider } from "@material-ui/core";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
