599d06d637ba964c908f1ba7adce604fb122b427
Thomas Soares <thomas__soares@hotmail.com>
2021-06-17 23:11:49 -0300

    Utilizando o componente Head.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,9 @@ import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Head>
+      <title>e-diaristas</title>
+    </Head>
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
     </ThemeProvider>
