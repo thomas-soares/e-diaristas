5cba8b1c9d27624c5e950d51f0508e9ce7582c11
Thomas Soares <thomas__soares@hotmail.com>
2021-06-18 22:33:04 -0300

    Passando o appbar pra ser estilizado.

--- src/ui/components/surfaces/Header/Header.style.tsx
+++ src/ui/components/surfaces/Header/Header.style.tsx
@@ -1,4 +1,4 @@
 import { experimentalStyled as styled } from "@material-ui/core/styles";
 import { AppBar } from "@material-ui/core";
 
-export const HeaderContainer = styled("div")``;
+export const HeaderAppBar = styled(AppBar)``;
