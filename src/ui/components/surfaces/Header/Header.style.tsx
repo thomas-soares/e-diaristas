587d9f8be589f986a640223edf64f6cb63dce6dc
Thomas Soares <thomas__soares@hotmail.com>
2021-06-19 20:24:51 -0300

    Estilizando toolbar em telas de desk.

--- src/ui/components/surfaces/Header/Header.style.tsx
+++ src/ui/components/surfaces/Header/Header.style.tsx
@@ -4,6 +4,12 @@ import { AppBar } from "@material-ui/core";
 export const HeaderAppBar = styled(AppBar)`
   background-color: ${({ theme }) => theme.palette.background.paper};
   box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.05);
+
+  ${({ theme }) => theme.breakpoints.up("md")} {
+    .MuiToolbar-root {
+      height: 100px;
+    }
+  }
 `;
 
 export const HeaderLogo = styled("img")``;
