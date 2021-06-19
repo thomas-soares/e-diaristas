f424d137fcac9d02f98c231b54708489c6b90aa5
Thomas Soares <thomas__soares@hotmail.com>
2021-06-19 20:21:42 -0300

    Estilizando o appbar do material ui.

--- src/ui/components/surfaces/Header/Header.style.tsx
+++ src/ui/components/surfaces/Header/Header.style.tsx
@@ -1,6 +1,9 @@
 import { experimentalStyled as styled } from "@material-ui/core/styles";
 import { AppBar } from "@material-ui/core";
 
-export const HeaderAppBar = styled(AppBar)``;
+export const HeaderAppBar = styled(AppBar)`
+  background-color: ${({ theme }) => theme.palette.background.paper};
+  box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.05);
+`;
 
 export const HeaderLogo = styled("img")``;
