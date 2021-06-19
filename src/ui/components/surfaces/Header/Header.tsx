49a5ae73d10be476638992db77bae45dcbe55601
Thomas Soares <thomas__soares@hotmail.com>
2021-06-18 22:40:51 -0300

    Importando e utilizando componente toolbar do material-ui.

--- src/ui/components/surfaces/Header/Header.tsx
+++ src/ui/components/surfaces/Header/Header.tsx
@@ -1,7 +1,12 @@
 import { HeaderAppBar } from "./Header.style";
+import { Toolbar } from "@material-ui/core";
 
 const Header: React.FC = () => {
-  return <HeaderAppBar position={"sticky"}>fseg</HeaderAppBar>;
+  return (
+    <HeaderAppBar position={"sticky"}>
+      <Toolbar>siehgiuse</Toolbar>
+    </HeaderAppBar>
+  );
 };
 
 export default Header;
