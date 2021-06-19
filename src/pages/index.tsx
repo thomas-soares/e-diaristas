aa6c405e02a30ec9837931311ca1c57cc9d2680d
Thomas Soares <thomas__soares@hotmail.com>
2021-06-19 20:52:55 -0300

    Importando e utilizando o componente UserInformation.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 export default function Home() {
   return (
     <div>
       <SafeEnvironment />
       <PageTitle
-        title={'Conheça os profissionais'}
-        subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
       />
+      <UserInformation />
     </div>
-  )
+  );
 }
