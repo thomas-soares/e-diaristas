113b870878c6db416c62cc244420d04e22952a30
Thomas Soares <thomas__soares@hotmail.com>
2021-06-18 22:38:10 -0300

    Passando o position sticky pro componente appbar.

--- src/ui/components/surfaces/Header/Header.tsx
+++ src/ui/components/surfaces/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { HeaderAppBar } from "./Header.style";
 
 const Header: React.FC = () => {
-  return <HeaderAppBar></HeaderAppBar>;
+  return <HeaderAppBar position={"sticky"}>fseg</HeaderAppBar>;
 };
 
 export default Header;
