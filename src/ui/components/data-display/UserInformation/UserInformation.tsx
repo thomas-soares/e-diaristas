34acb071e73f7ff57af28247d61329b9ce5cd14e
Thomas Soares <thomas__soares@hotmail.com>
2021-06-19 21:02:18 -0300

    Utilizando os componentes de estilo criados.

--- src/ui/components/data-display/UserInformation/UserInformation.tsx
+++ src/ui/components/data-display/UserInformation/UserInformation.tsx
@@ -1,5 +1,20 @@
+import {
+  UserInformationContainer,
+  UserName,
+  UserDescription,
+  AvatarStyled,
+  RatingStyled,
+} from "./UserInformation.style";
+
 const UserInformation: React.FC = (props) => {
-  return <div></div>;
+  return (
+    <UserInformationContainer>
+      <AvatarStyled />
+      <UserName>Thomas</UserName>
+      <RatingStyled />
+      <UserDescription />
+    </UserInformationContainer>
+  );
 };
 
 export default UserInformation;
