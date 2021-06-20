9ac836d86c9159a96b885a0d905b8a754529d1f6
Thomas Soares <thomas__soares@hotmail.com>
2021-06-19 21:01:48 -0300

    Criando componentes de estilo.

--- src/ui/components/data-display/UserInformation/UserInformation.style.tsx
+++ src/ui/components/data-display/UserInformation/UserInformation.style.tsx
@@ -1,1 +1,11 @@
 import { experimentalStyled as styled } from "@material-ui/core/styles";
+
+export const UserInformationContainer = styled("div")``;
+
+export const UserName = styled("div")``;
+
+export const UserDescription = styled("div")``;
+
+export const AvatarStyled = styled("div")``;
+
+export const RatingStyled = styled("div")``;
