331493689feec5d77e6ee32075cb7ce653d76ae2
Thomas Soares <thomas__soares@hotmail.com>
2021-06-20 23:08:46 -0300

    Font do username.

--- src/ui/components/data-display/UserInformation/UserInformation.style.tsx
+++ src/ui/components/data-display/UserInformation/UserInformation.style.tsx
@@ -3,7 +3,11 @@ import { Avatar, Rating } from "@material-ui/core";
 
 export const UserInformationContainer = styled("div")``;
 
-export const UserName = styled("div")``;
+export const UserName = styled("div")`
+  font-weight: bolder;
+  color: ${({ theme }) => theme.palette.text.primary};
+  font-size: ${({ theme }) => theme.typography.body2.fontSize};
+`;
 
 export const UserDescription = styled("div")``;
 
