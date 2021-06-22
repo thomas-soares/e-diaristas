81b5aa6b1ab7a4812e9b3d2d80e3ad87c923d3f7
Thomas Soares <thomas__soares@hotmail.com>
2021-06-21 22:32:26 -0300

    Espaçamento vertical do footer.

--- src/ui/components/surfaces/Footer/Footer.style.tsx
+++ src/ui/components/surfaces/Footer/Footer.style.tsx
@@ -4,4 +4,5 @@ export const FooterStyled = styled("footer")`
   background-color: ${({ theme }) => theme.palette.primary.main};
   color: ${({ theme }) =>
     theme.palette.getContrastText(theme.palette.primary.main)};
+  padding: ${({ theme }) => theme.spacing(4)} 0;
 `;
