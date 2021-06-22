88f8f67a69124c8ed2109f4ad329f18215f27c1b
Thomas Soares <thomas__soares@hotmail.com>
2021-06-21 22:27:34 -0300

    Passando cor de background do footer.

--- src/ui/components/surfaces/Footer/Footer.style.tsx
+++ src/ui/components/surfaces/Footer/Footer.style.tsx
@@ -1,3 +1,5 @@
 import { experimentalStyled as styled } from "@material-ui/core/styles";
 
-export const FooterStyled = styled("footer")``;
+export const FooterStyled = styled("footer")`
+  background-color: ${({ theme }) => theme.palette.primary.main};
+`;
