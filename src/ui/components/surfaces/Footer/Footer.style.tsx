220772422a9b7e7ab93f6f1376ca0acbf9b3608a
Thomas Soares <thomas__soares@hotmail.com>
2021-06-21 22:28:18 -0300

    Passando cor da font com funçao que analisa contraste do texto com o fundo.

--- src/ui/components/surfaces/Footer/Footer.style.tsx
+++ src/ui/components/surfaces/Footer/Footer.style.tsx
@@ -2,4 +2,6 @@ import { experimentalStyled as styled } from "@material-ui/core/styles";
 
 export const FooterStyled = styled("footer")`
   background-color: ${({ theme }) => theme.palette.primary.main};
+  color: ${({ theme }) =>
+    theme.palette.getContrastText(theme.palette.primary.main)};
 `;
