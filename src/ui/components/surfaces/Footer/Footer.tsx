9d528be036db798e603887373568f705255011c2
Thomas Soares <thomas__soares@hotmail.com>
2021-06-22 22:55:55 -0300

    Ajustando a marcaçao dos elementos.

--- src/ui/components/surfaces/Footer/Footer.tsx
+++ src/ui/components/surfaces/Footer/Footer.tsx
@@ -10,26 +10,30 @@ const Footer: React.FC = () => {
   return (
     <FooterStyled>
       <FooterContainer>
-        <FooterTitle>Quem somos</FooterTitle>
-        <Typography>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Necessitatibus eaque corporis est explicabo voluptatum, obcaecati
-          repudiandae voluptas, accusantium a quod veritatis rerum optio
-          temporibus ab similique unde sunt laborum iure?
-        </Typography>
-        <FooterTitle>Baixe nossos aplicativos</FooterTitle>
-        <AppList>
-          <li>
-            <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
-              <img src={"/img/logos/app-store.png"} alt={"AppStore"} />
-            </a>
-          </li>
-          <li>
-            <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
-              <img src={"/img/logos/google-play.png"} alt={"Google Play"} />
-            </a>
-          </li>
-        </AppList>
+        <div>
+          <FooterTitle>Quem somos</FooterTitle>
+          <Typography>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Necessitatibus eaque corporis est explicabo voluptatum, obcaecati
+            repudiandae voluptas, accusantium a quod veritatis rerum optio
+            temporibus ab similique unde sunt laborum iure?
+          </Typography>
+        </div>
+        <div>
+          <FooterTitle>Baixe nossos aplicativos</FooterTitle>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt={"AppStore"} />
+              </a>
+            </li>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/google-play.png"} alt={"Google Play"} />
+              </a>
+            </li>
+          </AppList>
+        </div>
       </FooterContainer>
     </FooterStyled>
   );
