09ffc88095a4dec0023da3433fe10635f0b03f7f
Thomas Soares <thomas__soares@hotmail.com>
2021-06-24 22:24:29 -0300

    Mudando o componente de input.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
-import TextField from "ui/components/inputs/TextField/TextField";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
-      <TextField label={"Digite seu CEP"} fullWidth />
+      <TextFieldMask label={"Digite seu CEP"} fullWidth />
       <UserInformation
         name={"Thomas"}
         picture={"https://github.com/thomas-soares.png"}
