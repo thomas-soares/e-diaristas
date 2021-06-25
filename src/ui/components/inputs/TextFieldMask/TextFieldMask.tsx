f5111f1b5c7e09ee4f69f96304ca590815cac987
Thomas Soares <thomas__soares@hotmail.com>
2021-06-24 22:31:52 -0300

    Criando interface do componente.

--- src/ui/components/inputs/TextFieldMask/TextFieldMask.tsx
+++ src/ui/components/inputs/TextFieldMask/TextFieldMask.tsx
@@ -1,6 +1,10 @@
 import InputMask from "react-input-mask";
 import TextField from "ui/components/inputs/TextField/TextField";
 
+export interface TextFieldMaskProps {
+  mask: string;
+}
+
 const TextFieldMask = (props) => {
   return (
     <InputMask mask={"99.999-999"}>
