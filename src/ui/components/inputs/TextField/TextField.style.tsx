a22bfa824716e5a00c56c24b8e2762e00f425446
Thomas Soares <thomas__soares@hotmail.com>
2021-06-24 22:14:38 -0300

    Cor de fundo do input.

--- src/ui/components/inputs/TextField/TextField.style.tsx
+++ src/ui/components/inputs/TextField/TextField.style.tsx
@@ -1,4 +1,8 @@
 import { experimentalStyled as styled } from "@material-ui/core/styles";
 import { TextField } from "@material-ui/core";
 
-export const TextFieldStyled = styled(TextField)``;
+export const TextFieldStyled = styled(TextField)`
+  .MuiInputBase-root {
+    background-color: ${({ theme }) => theme.palette.grey[50]};
+  }
+`;
