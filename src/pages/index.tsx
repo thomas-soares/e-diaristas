566b4265e1e30c6655112ff1377b82055ac9b761
Thomas Soares <thomas__soares@hotmail.com>
2021-06-25 22:52:32 -0300

    Especifificando cor do botao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,9 @@ export default function Home() {
         variant={"outlined"}
         fullWidth
       />
-      <Button variant={"contained"}>Buscar</Button>
+      <Button variant={"contained"} color={"secondary"}>
+        Buscar
+      </Button>
       <UserInformation
         name={"Thomas"}
         picture={"https://github.com/thomas-soares.png"}
