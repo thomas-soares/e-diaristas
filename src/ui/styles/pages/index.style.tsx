3b92f818281ce6afad45e73be39424a41750782f
Thomas Soares <thomas__soares@hotmail.com>
2021-06-26 21:07:16 -0300

    Estilo inicial do container do form

--- src/ui/styles/pages/index.style.tsx
+++ src/ui/styles/pages/index.style.tsx
@@ -1,3 +1,10 @@
 import { experimentalStyled as styled } from "@material-ui/core/styles";
 
-export const FormElementsContainer = styled("div")``;
+export const FormElementsContainer = styled("div")`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacing(5)};
+  max-width: 650px;
+  margin: 0 auto ${({ theme }) => theme.spacing(7)};
+`;
