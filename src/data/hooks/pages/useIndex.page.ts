86cddbc71f2938f07aa373b171e98aacdc7b0620
Thomas Soares <thomas__soares@hotmail.com>
2021-06-28 22:37:48 -0300

    Criaçao do estado de erro

--- src/data/hooks/pages/useIndex.page.ts
+++ src/data/hooks/pages/useIndex.page.ts
@@ -2,9 +2,12 @@ import { useState } from "react";
 
 export default function useIndex() {
   const [cep, setCep] = useState("");
+  const [error, setError] = useState("");
 
   return {
     cep,
     setCep,
+    error,
+    setError,
   };
 }
