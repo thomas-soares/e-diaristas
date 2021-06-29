cebb856f9499d8115930e5dff25d9bb857c143fb
Thomas Soares <thomas__soares@hotmail.com>
2021-06-28 22:38:25 -0300

    Criaçao de quando faz a busca

--- src/data/hooks/pages/useIndex.page.ts
+++ src/data/hooks/pages/useIndex.page.ts
@@ -3,11 +3,14 @@ import { useState } from "react";
 export default function useIndex() {
   const [cep, setCep] = useState("");
   const [error, setError] = useState("");
+  const [searchDone, setSearchDone] = useState(false);
 
   return {
     cep,
     setCep,
     error,
     setError,
+    searchDone,
+    setSearchDone,
   };
 }
